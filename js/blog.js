7d0875684da48035d326ef858bbf09479484a82a
Raju-Mia <rajucse00@gmail.com>
2022-02-18 16:28:57 +0600

    create blog page with used javaScript

---
+++ js/blog.js
@@ -0,0 +1,40 @@
+// document.write("This is our Javascript class");
+
+// let option = "this is simple line";
+// function myFunction(){
+//     let text = document.getElementById("demo").innerHTML;
+//     document.getElementById("demo").innerHTML = text.replace(option);
+// }
+
+
+
+function myFunction1() {
+    let fulltext = document.getElementById("ftext1").innerHTML;
+    let text = document.getElementById("demo1").innerHTML; 
+    document.getElementById("demo1").innerHTML = text.replace("...More Continue", fulltext);
+}
+
+
+function myFunction2() {
+    let fulltext = document.getElementById("ftext2").innerHTML;
+    let text = document.getElementById("demo2").innerHTML; 
+    document.getElementById("demo2").innerHTML = text.replace("...More Continue", fulltext);
+}
+
+function myFunction3() {
+    let fulltext = document.getElementById("ftext3").innerHTML;
+    let text = document.getElementById("demo3").innerHTML; 
+    document.getElementById("demo3").innerHTML = text.replace("...More Continue", fulltext);
+}
+
+function myFunction4() {
+    let fulltext = document.getElementById("ftext4").innerHTML;
+    let text = document.getElementById("demo4").innerHTML; 
+    document.getElementById("demo4").innerHTML = text.replace("...More Continue", fulltext);
+}
+
+function myFunction5() {
+    let fulltext = document.getElementById("ftext5").innerHTML;
+    let text = document.getElementById("demo5").innerHTML; 
+    document.getElementById("demo5").innerHTML = text.replace("...More Continue", fulltext);
+}
